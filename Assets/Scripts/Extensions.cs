52e6eb1886bf4e93210172834f9ac4a71d43ced8
thegreatpl <imperialjustinian@gmail.com>
2020-01-31 20:49:45 +0000

    Fileloader and beginnings of the maps saving and loading.

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.Tilemaps;
+
+public static partial class Extensions
+{
+    /// <summary>
+    /// Gets all the tiles as tiledata. 
+    /// </summary>
+    /// <param name="tilemap"></param>
+    /// <returns></returns>
+    public static IEnumerable<TileData> GetAllTileData(this Tilemap tilemap)
+    {
+        foreach(var pos in tilemap.cellBounds.allPositionsWithin)
+        {
+            if (tilemap.HasTile(pos))
+            {
+                var tile = tilemap.GetTile(pos);
+                yield return new TileData()
+                {
+                    Postion = pos,
+                    TileName = tile.name
+                }; 
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/Maps/TileData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine; 
+
+[Serializable]
+public class TileData
+{
+    /// <summary>
+    /// The location of this tile. 
+    /// </summary>
+    public Vector3Int Postion;
+
+    /// <summary>
+    /// The name of the tile that is in this position. 
+    /// </summary>
+    public string TileName; 
+}
+
