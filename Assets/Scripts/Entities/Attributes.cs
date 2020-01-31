9de3379b925772da1cb8879a249354e750d8c012
Joshua Conroy-Howes <44055993+OGFameless@users.noreply.github.com>
2020-01-31 22:34:17 +0000

    Movement/Attributes
    Setup of movement/attributes

---
+++ Assets/Scripts/Entities/Attributes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+[Serializable]
+public class Attributes
+{
+    public float Speed; //Variable for speed
+    int MaxHP; //Variable for MaxHP;
+    int Strength; //Variable for strength
+    int Dexterity; //Variable for dex
+    int Intellect; //Variable for int
+
+
+
+public class Attribute : MonoBehaviour
+{
+    public int CurrentHP;
+
+    public Attributes attributes;
+    
+        public void AssignAttributes(Attributes attribute) 
+        {
+            attribute = attributes;
+            CurrentHP = attribute.MaxHP;
+        }
+
+
+
+    }
---
+++ Assets/Scripts/Entities/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Attributes attributes; //Reference to Attributes script
+ 
+    void Awake() 
+    {
+        Attributes attributes = GetComponent<Attributes>(); //Setup reference
+    }
+
+    void MoveUp() //Move upwards
+    {
+        transform.Translate(Vector2.up * attributes.Speed);
+    }
+
+    void MoveDown()  //Move downwards
+    {
+        transform.Translate(-Vector2.up * attributes.Speed);
+    }
+
+    void MoveLeft() //Move left
+    {
+        transform.Translate(-Vector2.right * attributes.Speed);
+    }
+
+    void MoveRight() //Move right
+    {
+        transform.Translate(Vector2.right * attributes.Speed);
+    }
+}
