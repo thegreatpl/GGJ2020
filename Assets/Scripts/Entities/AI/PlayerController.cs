62f3623f8229bcd6364388aa1666439e9a451043
thegreatpl <imperialjustinian@gmail.com>
2020-02-01 04:54:29 +0000

    Player movement done and decent speed as well.

--- Assets/Scripts/Entities/AI/PlayerController.cs
+++ Assets/Scripts/Entities/AI/PlayerController.cs
@@ -19,21 +19,28 @@ public class PlayerController : MonoBehaviour
         var moveY = Input.GetAxis("Vertical"); 
         if (moveX <0 )
         {
+            Movement.Direction = Direction.Left; 
             Movement.MoveLeft(); 
         }
         else if (moveX > 0)
         {
+            Movement.Direction = Direction.Right;
             Movement.MoveRight(); 
         }
 
         if (moveY < 0)
         {
+            Movement.Direction = Direction.Down;
             Movement.MoveDown(); 
         }
         else if (moveY > 0)
         {
+            Movement.Direction = Direction.Up;
             Movement.MoveUp();
         }
+
+        if (moveX == 0 && moveY == 0)
+            Movement.Direction = Direction.None; 
 
 
     }
--- Assets/Scripts/Entities/Movement.cs
+++ Assets/Scripts/Entities/Movement.cs
@@ -14,27 +14,51 @@ public class Movement : MonoBehaviour
          Attribute = GetComponent<Attribute>(); //Setup reference
     }
 
-    public void MoveUp() //Move upwards
+
+    void Update()
     {
-        transform.position += new Vector3(0, Attribute.attributes.Speed);//  .Translate(Vector2.up * Attribute.attributes.Speed);
+        switch (Direction)
+        {
+            case Direction.None:
+                break;
+            case Direction.Left:
+                transform.position += new Vector3(-Attribute.attributes.Speed, 0) * Time.deltaTime ;
+                break;
+            case Direction.Up:
+                transform.position += new Vector3(0, Attribute.attributes.Speed) * Time.deltaTime;
+                break;
+            case Direction.Right:
+                transform.position += new Vector3(Attribute.attributes.Speed, 0) * Time.deltaTime;
+                break;
+            case Direction.Down:
+                transform.position += new Vector3(0, -Attribute.attributes.Speed) * Time.deltaTime;
+                break;
+        }
+    }
+
+
+        public void MoveUp() //Move upwards
+    {
+        //transform.position += new Vector3(0, Attribute.attributes.Speed);//  .Translate(Vector2.up * Attribute.attributes.Speed);
     }
 
     public void MoveDown()  //Move downwards
     {
         //transform.Translate(-Vector2.up * Attribute.attributes.Speed);
-        transform.position += new Vector3(0, -Attribute.attributes.Speed);
+       // transform.position += new Vector3(0, -Attribute.attributes.Speed);
     }
 
     public void MoveLeft() //Move left
     {
-        transform.position += new Vector3( -Attribute.attributes.Speed, 0);
+       // transform.position += new Vector3( -Attribute.attributes.Speed, 0);
        // transform.Translate(-Vector2.right * Attribute.attributes.Speed);
     }
 
     public void MoveRight() //Move right
     {
-        transform.position += new Vector3(Attribute.attributes.Speed, 0);
+      //  transform.position += new Vector3(Attribute.attributes.Speed, 0);
 
        // transform.Translate(Vector2.right * Attribute.attributes.Speed);
     }
+    
 }
