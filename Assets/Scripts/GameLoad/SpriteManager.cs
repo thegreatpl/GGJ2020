02ba0f4327de37d7eee78e27e88b340af81c33af
thegreatpl <imperialjustinian@gmail.com>
2020-02-01 13:37:57 +0000

    moved the character sprites into streaming assets.

--- Assets/Scripts/GameLoad/SpriteManager.cs
+++ Assets/Scripts/GameLoad/SpriteManager.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO; 
+using System.IO;
+using System;
 
 public class SpriteManager : MonoBehaviour
 {
@@ -30,6 +31,35 @@ public class SpriteManager : MonoBehaviour
 
     public IEnumerator LoadSprites()
     {
-        yield return null; 
+        var files = Directory.GetFiles($"{FileLoader.ModPath}/{SpritePath}", "*.png", SearchOption.AllDirectories);
+        int count = 0; 
+        foreach(var file in files)
+        {
+            var texture = FileLoader.LoadTexture2D(file);
+            var name = Path.GetFileNameWithoutExtension(file);
+
+
+            for (int xdx = 0; xdx <= texture.width; xdx += 64)
+            {
+                for (int ydx = 0; ydx <= texture.height; ydx += 64)
+                {
+                    try
+                    {
+                        var sprite = Sprite.Create(texture, new Rect(xdx, ydx, 64, 64), new Vector2(0.5f, 0.5f), 32);
+                        sprite.name = $"{name}_{xdx}_{ydx}";
+                        Sprites.Add(sprite.name, sprite);
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.Log($"Error loading tile: {e.Message}");
+                    }
+                }
+            }
+            count++; 
+            if (count % 10 == 0)
+                yield return null; 
+        }
+
+        //yield return null; 
     }
 }
